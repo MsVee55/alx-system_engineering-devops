e4acfe1243f8aed31dd86eb0519d4cb638a48df8
MsVee55 <precious.v.augustine@gmail.com>
2023-05-09 15:53:27 -0700

    API advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -14,7 +14,7 @@ def count_words(subreddit, word_list, found_list=[], after=None):
     '''
     user_agent = {'User-agent': 'test45'}
     posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
-                        .format(subreddit, after), headers=user_agent)
+                         .format(subreddit, after), headers=user_agent)
     if after is None:
         word_list = [word.lower() for word in word_list]
 
@@ -24,8 +24,9 @@ def count_words(subreddit, word_list, found_list=[], after=None):
         posts = posts['children']
         for post in posts:
             title = post['data']['title'].lower()
-            for word in word_list:
-                 found_list.append(word)
+            for word in title.split(' '):
+                if word in word_list:
+                    found_list.append(word)
         if aft is not None:
             count_words(subreddit, word_list, found_list, aft)
         else:
@@ -36,7 +37,7 @@ def count_words(subreddit, word_list, found_list=[], after=None):
                 else:
                     result[word.lower()] = 1
             for key, value in sorted(result.items(), key=lambda item: item[1],
-                                    reverse=True):
+                                     reverse=True):
                 print('{}: {}'.format(key, value))
     else:
         return
